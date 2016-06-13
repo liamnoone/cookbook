2159d300b32b1440db96c3106d1c7b703f18796c
Liam Noone <liam@liamnoone.net>
2016-06-13 10:46:00 +0100

    Switch DB adapter to Postgres

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -16,7 +16,7 @@ Hanami::Model.configure do
   #    adapter type: :sql, uri: 'postgres://localhost/cookbook_development'
   #    adapter type: :sql, uri: 'mysql://localhost/cookbook_development'
   #
-  adapter type: :file_system, uri: ENV['COOKBOOK_DATABASE_URL']
+  adapter type: :sql, uri: ENV['COOKBOOK_DATABASE_URL']
 
   ##
   # Database mapping
